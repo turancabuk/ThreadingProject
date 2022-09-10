9c1eb860a3e07655c27efc1e9ca14c006489efc2
turan çabuk <turan6677@gmail.com>
2022-09-10 17:26:47 +0300

    Initial Commit

---
+++ ThreadingProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ThreadingProject
+//
+//  Created by Turan Çabuk on 10.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
