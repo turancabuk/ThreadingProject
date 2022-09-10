8c7b782c548930ba118d806490ccb4dc808f04f3
turan çabuk <turan6677@gmail.com>
2022-09-10 18:40:41 +0300

    imageString added. changePhoto func added.

--- ThreadingProject/ViewController.swift
+++ ThreadingProject/ViewController.swift
@@ -10,16 +10,35 @@ import UIKit
 class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
 
     @IBOutlet weak var imageView: UIImageView!
-    
     @IBOutlet weak var tableView: UITableView!
     
+    let imageString = ["https://images.hdqwalls.com/wallpapers/skye-united-kingdom-8k-yh.jpg","https://images.hdqwalls.com/wallpapers/lone-tree-oak-5k-bx.jpg"]
+    var data = Data()
+    var tracker = 0
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.delegate = self
         tableView.dataSource = self
+        
+        data = try! Data(contentsOf: URL(string: imageString[tracker])!)
+        imageView.image = UIImage(data: data)
        
+        
+        self.navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.action, target: self, action: #selector(changePhoto))
+    }
+    @objc func changePhoto(){
+        
+        if tracker == 0 {
+            tracker += 1
+        }else{
+            tracker -= 1
+        }
+        
+        data = try! Data(contentsOf: URL(string: imageString[tracker])!)
+        imageView.image = UIImage(data: data)
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 48
